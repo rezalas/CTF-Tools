40d2d123c4146e6430459d951b349ac482a7ca49
Paul McDowell <codlordpaul@hotmail.com>
2023-02-02 15:53:30 -0600

    update comments to clarify intent

--- numToString.py
+++ numToString.py
@@ -2,29 +2,33 @@ import sys
 import base64
 import re
 
+##
+## Author: Paul McDowell
+##
+## License: MIT
+##
+
+
 def parseAsNum(num:str): 
-    if re.search(r'[^0-1\ ]', num) == None: # binary
+    if re.search(r'[^0-1\ ]', num) == None: # binary number of some kind.
         tmpSplit = num.split(' ')
         bArray = bytearray()
-        for bNum in tmpSplit:
+        for bNum in tmpSplit: # loops through binary character array to rebuild the ascii string
             bArray.append(int(bNum, 2))
         
         num = bArray.decode('utf-8')
         print('From binary: ' + num)
     if num.startswith("0x"): # hex encoded probably
         print('From Hex: ' + bytes.fromhex(num[2:]).decode('utf-8'))
-    
+    # detects a base64 pattern, but not guaranteed to actually BE base64.
     if re.search(r'^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?$',num) != None:
-        try:
+        try: # maybe base64 ? looping through decode/encode and decode ascii will determine if it's valid
             if base64.b64encode(base64.b64decode(num)).decode('utf-8') == num: 
-                # maybe base64 ?
                 print('From B64: ' + base64.b64decode(num).decode('utf-8'))
         except Exception:
             return
-    else:
-        print('Post processed guess: ' + num)
 
-if len(sys.argv) < 2:
-    raise ValueError("Please provide the number to convert as a parameter.")
+if len(sys.argv) != 2:
+    raise ValueError("Please provide a single string-form number or binary array to convert as the only parameter.")
 
 parseAsNum(sys.argv[1])
